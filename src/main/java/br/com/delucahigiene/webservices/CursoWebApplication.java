63f07b9936a6a0bc329375e028104526e6b677ca
Rafael De Luca <deluca1712@gmail.com>
2021-05-14 14:04:58 -0300

    Projeted created

---
+++ src/main/java/br/com/delucahigiene/webservices/CursoWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.delucahigiene.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoWebApplication.class, args);
+	}
+
+}
