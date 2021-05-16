82a6126fdc2637c1a8e96a3120b59e04a9f84ea6
Rafael De Luca <deluca1712@gmail.com>
2021-05-15 21:33:23 -0300

    Component registration and service layer

---
+++ src/main/java/br/com/delucahigiene/webservices/services/UserService.java
@@ -0,0 +1,27 @@
+package br.com.delucahigiene.webservices.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.delucahigiene.webservices.entities.User;
+import br.com.delucahigiene.webservices.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
